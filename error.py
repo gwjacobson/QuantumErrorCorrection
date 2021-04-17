05c27553bf163e93ec3722ba0adf2815cbdc230a
gwjacobson <graemejacobson@gmail.com>
2021-04-17 16:24:36 -0500

    Added more files and started error simulation/single qubit case

--- error.py
+++ error.py
@@ -1,6 +1,22 @@
 from qiskit import *
+import random
 
-## Generation of a random qubit error.
-## Qubit errors can be defined as a complex
-## linear cobimination of the Pauli matrices.
+## Simulation of a random error. When an error is place with some probability,
+## the function will simulation a measurement selection of a Pauli error,
+## I,X,Y,Z.
 
+#error will choose a Pauli error each with 25% prob
+def error(circuit, qubit):
+    error = random.randint(1,4) #25% chance for each selection
+
+    if error == 1:
+        circuit.id(0) #no error
+    elif error == 2:
+        circuit.x(qubit) #bit flip error
+    elif error == 3:
+        circuit.z(qubit) #phase flip error
+    else:
+        circuit.x(qubit) #combined phase and bit flip
+        circuit.z(qubit)
+
+    return
--- single_qubit.py
+++ single_qubit.py
@@ -1,20 +1,31 @@
 from qiskit import *
+from error import error
+from qiskit.providers.aer import extensions
 import random
 
-##Single qubit being exposed to random errors.
-##Loop qubit state 10 times, with 10% chance of
-##error after each step.
+##Single qubit being exposed to random errors from noise model.
+
+sim = Aer.get_backend('qasm_simulator')
 
 #inital quantum state
-q = QuantumCircuit(1, 1);
+q = QuantumCircuit(1,1);
+
+q.h(0)
+
+s1 = qiskit.quantum_info.DensityMatrix(q)
+
 
 for i in range(1,10):
-    prob = random.randint(1, 11)
+    prob = random.randint(1, 15)
 
     if prob == 1:
-        q.x(0)
+        error(q, 0)
         q.barrier(0)
     else:
         q.barrier(0)
+
+    s2 = qiskit.quantum_info.DensityMatrix(q)
+
+    print(qiskit.quantum_info.state_fidelity(s1, s2))
 
 print(q)
